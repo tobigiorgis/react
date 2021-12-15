694427a18b8af5d4ff3d49a266c226214fdecebc
tobigiorgis <90532007+tobigiorgis@users.noreply.github.com>
2021-12-14 23:54:12 -0300

    2 commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,14 @@ import React from "react"
 
 const NavBar = () => {
     return (
-            <h3>Barra de nav</h3>
+        <div>
+            <h1 className="nombre">Estantes Pliego Design</h1>
+            <p className="links"><a href= "./home.html">HOME</a></p>
+            <p className="links"><a href= "./catalogo.html">CATALOGO</a></p>
+        </div>
     )
 }
 
 
 export default NavBar
 
-import React from 'react'
